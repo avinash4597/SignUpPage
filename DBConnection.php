bd510f426f291f332eb977b46351bfb04be59b02
Avinash Paruchuri <38564240+avinash4597@users.noreply.github.com>
2020-04-10 19:48:18 +0530

    Add files via upload

---
+++ DBConnection.php
@@ -0,0 +1,12 @@
+//DBConnection.php
+
+<?php
+ //Establishing connection with the database
+ $DB = "localhost:3306";
+ $user ="root";
+ $password= 'root123';
+ $database='profile'; //where customers is the database
+$db = mysqli_connect('host, $user,$password);
+
+?>
+
